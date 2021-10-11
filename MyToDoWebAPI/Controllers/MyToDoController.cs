17c9e87dce944fc817c19b37ebadb7278433d639
Lorenz <lorenz.kassewalder95@gmail.com>
2021-10-11 14:27:33 +0200

    init

--- MyToDoWebAPI/Controllers/MyToDoController.cs
+++ MyToDoWebAPI/Controllers/MyToDoController.cs
@@ -23,8 +23,8 @@ namespace MyToDoWebAPI.Controllers
             return Ok(items);
         }
         [HttpGet]
-        [Route("{index}")]
-        public IActionResult GetSpecifiedItems(int index)
+        [Route("{index}", Name = "GetSpecificItem")]
+        public IActionResult GetSpecificItem(int index)
         {
             if(index < 0 || index > items.Count - 1)
             {
@@ -38,7 +38,41 @@ namespace MyToDoWebAPI.Controllers
         [HttpPost]
         public IActionResult AddItem([FromBody] string item)
         {
-
+            items.Add(item);
+            return CreatedAtRoute("GetSpecificItem", new { index = items.IndexOf(item) }, item);
+        }
+        [HttpDelete]
+        [Route("{index}")]
+        public IActionResult DeleteItem(int index)
+        {
+            if (index < 0 || index > items.Count - 1)
+            {
+                return BadRequest("Invalid Index.");
+            }
+            items.RemoveAt(index);
+            return NoContent();
+        }
+        [HttpPut]
+        [Route("{index}")]
+        public IActionResult UpdateItem(int index,[FromBody] string item)
+        {
+            if (index < 0 || index > items.Count - 1)
+            {
+                return BadRequest("Invalid Index.");
+            }
+            items[index] = item;
+            return Ok();
+        }
+        [HttpGet] // /api/todo-items/sorted?sortOrder=asc
+        [Route("sorted")]
+        public IActionResult GetAllItemsSorted([FromQuery]string sortOrder)
+        {
+            return sortOrder switch
+            {
+                "desc" => Ok(items.OrderByDescending(item => item)),
+                "asc" => Ok(items.OrderBy(item => item)),
+                _ => BadRequest("Invalid or missing sortorder query paramter")
+            };
         }
     }
 }
