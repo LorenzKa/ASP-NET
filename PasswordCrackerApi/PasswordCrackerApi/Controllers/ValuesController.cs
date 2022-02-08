6bd84a55df47f890c3e9b9dbb9875757c67c4e85
Lorenz <lorenz.kassewalder95@gmail.com>
2022-02-08 15:08:46 +0100

    Finished Algo with 1 Thread

--- PasswordCrackerApi/PasswordCrackerApi/Controllers/ValuesController.cs
+++ PasswordCrackerApi/PasswordCrackerApi/Controllers/ValuesController.cs
@@ -4,13 +4,12 @@ using PasswordCrackerApi.Dtos;
 
 namespace PasswordCrackerApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        [Route("test")]
         [HttpGet]
-        public async Task<IActionResult> test()
+        public async Task<IActionResult> Password0()
         {
             var worker = new Worker();
             var crackRequest = new CrackRequestDto()
@@ -18,6 +17,34 @@ namespace PasswordCrackerApi.Controllers
                 Alphabet = "abcdefghijklmnopqrstuvwxyz",
                 HashCode = "A746222F09D85605C52D4E636788D6FFDC274698B98B8C5F3244C06958683A69",
                 Length = 4
+            };
+            var resultTask = worker.BruteforcePool(crackRequest);
+            var result = await resultTask;
+            return Ok(result);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Password1()
+        {
+            var worker = new Worker();
+            var crackRequest = new CrackRequestDto()
+            {
+                Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                HashCode = "3086E346353248775A2C5D74E36A9C9B9BD226A1EE401F830AC499633DC00031",
+                Length = 6
+            };
+            var resultTask = worker.BruteforcePool(crackRequest);
+            var result = await resultTask;
+            return Ok(result);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Password2()
+        {
+            var worker = new Worker();
+            var crackRequest = new CrackRequestDto()
+            {
+                Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+                HashCode = "26775436073E00D207E192857EE3730CFCA19DE16F01F0780096EF217C2919EF",
+                Length = 5
             };
             var resultTask = worker.BruteforcePool(crackRequest);
             var result = await resultTask;
--- PasswordCrackerApi/PasswordCrackerApi/Worker.cs
+++ PasswordCrackerApi/PasswordCrackerApi/Worker.cs
@@ -14,42 +14,61 @@ namespace PasswordCrackerApi
         }
         public async Task<string> BruteforceUnit(string passwordHash, int length, char[] alphabet)
         {
-            char[] password = new string(alphabet[0], 4).ToCharArray();
-            Task<string> task = Task.Run(() => recursiveMethod(passwordHash, password, length, alphabet, 1, 1));
+            char[] password = new string(alphabet[0], length).ToCharArray();
+            Task<string> task = Task.Run(() => recursiveMethod(passwordHash.ToUpper(), password, alphabet, 0, 1));
             return await task;
 
-        }
-        public string recursiveMethod(string passwordHashToFind, char[] passwordArray, int length, char[] alphabet, int passwordIndexToChange, int alphabetIndex)
-        {
-            Console.WriteLine(alphabetIndex + "," + alphabet.Length + "," + passwordIndexToChange + "," + length);
-            if (alphabetIndex == alphabet.Length - 1 && passwordIndexToChange == 0) { Console.WriteLine("stopped"); return "not found"; };
-            if (passwordIndexToChange != 0)
-            {
-                passwordArray[passwordIndexToChange] = alphabet[alphabetIndex];
-                passwordIndexToChange++;
-            }
-            if (passwordIndexToChange == 0)
-            {
-                alphabetIndex++;
-                passwordArray[0] = alphabet[alphabetIndex];
-                passwordIndexToChange++;
-            }
-            
-            if (passwordIndexToChange == length)
-            {
-                passwordIndexToChange = 0;
-            }
-            Console.WriteLine(new string(passwordArray));
-            if (hashPassword(new string(passwordArray)) == passwordHashToFind) return new string(passwordArray);
-            Console.WriteLine(DateTime.Now.ToString());
-            recursiveMethod(passwordHashToFind, passwordArray, length, alphabet, passwordIndexToChange, alphabetIndex);
-            return "not found";
         }
         public string hashPassword(string password)
         {
             using (SHA256 mySHA256 = SHA256.Create())
             {
-                return System.Text.Encoding.Default.GetString(mySHA256.ComputeHash(Encoding.UTF8.GetBytes(password)));
+                //Console.WriteLine("Password: "+password);
+                var hash = mySHA256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                string result = "";
+                for (int i = 0; i < hash.Length; i++)
+                {
+                    result += hash[i].ToString("X2");
+                }
+                //Console.WriteLine("Hash: " + result);
+                return result;
+            }
+        }
+        public string recursiveMethod(string passwordHashToFind, char[] passwordArray, char[] alphabet, int passwordIndexToChange, int alphabetIndex)
+        {
+            //Console.WriteLine(alphabetIndex + "," + alphabet.Length + "," + passwordIndexToChange + "," + length+","+fixedPasswordIndex+","+fixedBruteforceIndex);
+            while (true)
+            {
+                if (hashPassword(new string(passwordArray)) == passwordHashToFind) return "Password: " + new string(passwordArray);
+                if (passwordArray.Contains(alphabet.Last()) && passwordArray.Distinct().Count() == 1) return "not found";
+                if (alphabetIndex == alphabet.Length)
+                {
+                    //Console.WriteLine("Reached a z");
+                    for (int offset = 1; offset < passwordArray.Length; offset++)
+                    {
+                        //Console.WriteLine(offset);
+                        if (Array.IndexOf(alphabet, passwordArray[offset]) != alphabet.Length - 1)
+                        {
+                            //Console.WriteLine("Went into if");
+                            //Console.WriteLine(passwordArray[offset]);
+                            //Console.WriteLine("Alphabetindex: " + (Array.IndexOf(alphabet, passwordArray[offset])));
+                            passwordArray[offset] = alphabet[Array.IndexOf(alphabet, passwordArray[offset]) + 1];
+                            for (int i = 0; i < offset; i++)
+                            {
+                                passwordArray[i] = alphabet.First();
+                            }
+                            //Console.WriteLine("Manipulated PW:" + new string(passwordArray));
+                            alphabetIndex = 0;
+                            //Console.WriteLine("Going out of for");
+                            break;
+                        }
+                    }
+                    //Console.WriteLine(passwordArray);
+
+                }
+                passwordArray[passwordIndexToChange] = alphabet[alphabetIndex];
+                Console.WriteLine(new string(passwordArray));
+                alphabetIndex++;
             }
         }
     }
