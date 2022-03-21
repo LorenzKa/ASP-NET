57c821bd5c3299fa4a0e03770f7f13d75a76dbc3
Lorenz <lorenz.kassewalder95@gmail.com>
2022-03-21 13:43:42 +0100

    Cleanup branch

--- PasswordCrackerApi/PasswordCrackerApi/Dtos/CrackRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PasswordCrackerApi.Dtos
-{
-    public class CrackRequestDto
-    {
-        public string? HashCode { get; set; }
-        public string? Alphabet { get; set; }
-        public int Length { get; set; }
-    }
-}
--- PasswordCrackerApi/PasswordCrackerApi/ProgressModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PasswordCrackerApi
-{
-    public class ProgressModel
-    {
-        public char Id { get; set; }
-        public int ProgressInPercent { get; set; }
-    }
-}
